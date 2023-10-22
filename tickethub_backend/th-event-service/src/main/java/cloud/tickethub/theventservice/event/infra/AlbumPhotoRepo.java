f70b8fe400552288e2794d40e78e0148e2df61f9
soheil babadi <babadishyl@gmail.com>
2023-10-22 12:19:26 +0330

    add repository annotation

--- tickethub_backend/th-event-service/src/main/java/cloud/tickethub/theventservice/event/infra/AlbumPhotoRepo.java
+++ tickethub_backend/th-event-service/src/main/java/cloud/tickethub/theventservice/event/infra/AlbumPhotoRepo.java
@@ -3,9 +3,11 @@ package cloud.tickethub.theventservice.event.infra;
 import cloud.tickethub.theventservice.event.domain.AlbumPhoto;
 import cloud.tickethub.theventservice.event.domain.EventAlbum;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AlbumPhotoRepo extends JpaRepository<AlbumPhoto, Long> {
 
     List<AlbumPhoto> findAllByEventAlbumOrderByCreatedByDesc(EventAlbum album);
--- tickethub_backend/th-event-service/src/main/java/cloud/tickethub/theventservice/event/infra/CategoryRepo.java
+++ tickethub_backend/th-event-service/src/main/java/cloud/tickethub/theventservice/event/infra/CategoryRepo.java
@@ -2,6 +2,8 @@ package cloud.tickethub.theventservice.event.infra;
 
 import cloud.tickethub.theventservice.event.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepo extends JpaRepository<Category, Long> {
 }
