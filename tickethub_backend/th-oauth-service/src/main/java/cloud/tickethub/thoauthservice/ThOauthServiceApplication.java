8e32e82cde0c32952a90d48138cbc1748768bfca
soheil babadi <babadishyl@gmail.com>
2023-10-23 10:10:02 +0330

    add oauth server

---
+++ tickethub_backend/th-oauth-service/src/main/java/cloud/tickethub/thoauthservice/ThOauthServiceApplication.java
@@ -0,0 +1,13 @@
+package cloud.tickethub.thoauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThOauthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThOauthServiceApplication.class, args);
+	}
+
+}
---
+++ tickethub_backend/th-oauth-service/src/test/java/cloud/tickethub/thoauthservice/ThOauthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.tickethub.thoauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThOauthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
